e25148515f57a3d7a06f4502cbd027d5a333e18e
k1m743hyun <k1m743hyun@icloud.com>
2025-06-09 16:11:52 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import pymysql
+from mysql.connector.aio.charsets import charsets
+
+
+def execute_db(path):
+    db = pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='order',
+        passwd='order',
+        db='order_db',
+        charset='utf8'
+    )
+
+    try:
+        with db.cursor() as cursor:
+            with open(path, 'r') as file:
+                for sql in file.read().split(';')[:-1]:
+                    print(sql)
+                    cursor.execute(sql)
+                    db.commit()
+                    print('=' * 50)
+    finally:
+        db.close()
+
+
+if __name__ == '__main__':
+    execute_db('./init/schema.sql')
+    execute_db('./init/data.sql')
